cf4ec084c97168f89e652cda91a0fdd61a900585
Mikkel RINGAUD <mikkel@milescode.dev>
2022-05-01 17:53:33 +0200

    feat(Arch-Linux): add bash and xorg to script

--- setup-arch-linux.sh
+++ setup-arch-linux.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 echo "-- Welcome to the Arch-Linux setup of Vexcited's dotfiles."
-echo "Configuration from https://github.com/vexcited/dotfiles"
-echo ""
+echo -e "Configuration from https://github.com/vexcited/dotfiles\n"
 
 echo "Checking if dotfiles directory exists..." 
 DOTFILES=$HOME/.vexcited-dotfiles/arch-linux
@@ -21,24 +20,33 @@ sudo pacman -S \
   discord \
   alacritty
 
-
 echo "Installing yay..."
 sudo pacman -S --needed base-devel git
 git clone https://aur.archlinux.org/yay-git.git && cd yay-git
 makepkg -si && cd .. && rm -rf yay-git
 
 echo "Installing Hack nerd font..."
-sudo yay -S nerd-fonts-hack
+yay -S nerd-fonts-hack
 
 echo "Login to GitHub... Follow the instructions given by the GitHub CLI."
 gh auth login
 
 echo "Symlinking dotfiles..."
 
+# Xorg
+echoe -e "\tSyncing xorg..."
+ln -s $DOTFILES/xorg/Xresources $HOME/.Xresources
+ln -s $DOTFILES/xorg/xinitrc $HOME/.xinitrc
+
 # Alacritty
+echo -e "\tSyncing alacritty..."
 mkdir -p $HOME/.config/alacritty
 ln -s $DOTFILES/alacritty/alacritty.yml $HOME/.config/alacritty/.alacritty.yml
 
+# Bash
+echo -e "\tSyncing bash..."
+ln -s $DOTFILES/bash/bashrc $HOME/.bashrc
+ln -s $DOTFILES/bash/bash_profile $HOME/.bash_profile
 
 echo "Done !"
 exit 0
