d887a757c47ea252ef2b0e0100ed1231f22af60e
Mikkel RINGAUD <mikkel@milescode.dev>
2022-05-11 22:24:52 +0200

    feat(Arch-Linux): trying to implement more checks for setup script

--- setup-arch-linux.sh
+++ setup-arch-linux.sh
@@ -5,14 +5,10 @@ echo -e "Configuration from https://github.com/vexcited/dotfiles\n"
 
 echo "Checking if dotfiles directory exists..." 
 DOTFILES=$HOME/.vexcited-dotfiles/arch-linux
-if [ ! -d "$DOTFILES" ];
-then
+if [ ! -d "$DOTFILES" ]; then
   echo "$DOTFILES directory doesn't exist ! Make sure you've cloned into $DOTFILES, else this script won't work."
   exit 1
 fi
-
-
-
 
 echo "Installing pacman packages... (it will maybe ask you a password)"
 
@@ -56,78 +52,116 @@ for package in ${packages_to_install[@]}; do
   install_package $package
 done
 
-echo "Installing yay..."
-git clone https://aur.archlinux.org/yay-git.git && \ # Cloning...
-cd yay-git && makepkg -si && \ # Installation...
-cd .. && rm -rf yay-git # Cleaning...
+if ! command -v yay &> /dev/null; then
+  echo "'yay' not found. Installing it..."
+
+  git clone https://aur.archlinux.org/yay-git.git && \
+  cd yay-git && makepkg -si && \
+  cd .. && rm -rf yay-git
+fi
 
 echo "Installing Hack Nerd Font..."
 yay -S nerd-fonts-hack
 
-echo "Login to GitHub... Follow the instructions given by the GitHub CLI."
-gh auth login
+gh auth status &> /dev/null
+if [[ $? != 0 ]]; then
+  echo "Login to GitHub... Follow the instructions given by the GitHub CLI."
+  gh auth login
+fi
 
-echo "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+if [ ! -d $HOME/.oh-my-zsh ]; then
+  echo "Installing oh-my-zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
-echo "Installing powerlevel10k omz theme..."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$Hcustom}/themes/powerlevel10k
+if [ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]; then
+  echo "Installing powerlevel10k oh-my-zsh custom theme..."
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$Hcustom}/themes/powerlevel10k
+fi
 
-echo "Installing NVM..."
-yay -S nvm
+if ! command -v nvm &> /dev/null; then
+  echo "Installing NVM..."
+  yay -S nvm
+fi
 
-echo "Installing Google Chrome"
-yay -S google-chrome
+if ! command -v google-chrome-stable &> /dev/null; then
+  echo "Installing Google Chrome Stable"
+  yay -S google-chrome
+fi
 
 echo "Symlinking dotfiles..."
+echo "Warning: already existing configurations will be removed !"
 mkdir -p $HOME/.config
+
+create_symlink () {
+  dotfilesLink=$1
+  targetLink=$2
+
+  if [[ -L "$targetLink" ]]; then
+    if [[ -e "$targetLink" ]]; then
+      if [[ $(readlink $targetLink) == $dotfilesLink ]]; then
+        echo "'$targetLink' already exists. Skipping..."
+      else
+	echo "'$targetLink' is alrady linked to another file. Replacing it..."
+	unlink $targetLink
+	ln -sf $dotfilesLink $targetLink
+      fi
+    else
+      echo "'$targetLink' link was broken. Creating a new one..."
+      unlink $targetLink
+      ln -s $dotfilesLink $targetLink
+    fi
+  else
+    echo "'$targetLink' doesn't exists. Creating it..."
+    ln -s $dotfilesLink $targetLink
+  fi
+}
 
 # Xorg
 echo -e "\tSyncing xorg..."
-ln -s $DOTFILES/xorg/Xresources $HOME/.Xresources
-ln -s $DOTFILES/xorg/xinitrc $HOME/.xinitrc
+create_symlink $DOTFILES/xorg/Xresources $HOME/.Xresources
+create_symlink $DOTFILES/xorg/xinitrc $HOME/.xinitrc
 
 # Alacritty
-echo -e "\tSyncing alacritty... (directory - in ~/.config/alacritty - is removed)"
-rm -rf $HOME/.config/alacritty
-ln -s $DOTFILES/alacritty $HOME/.config/alacritty
+echo -e "\tSyncing alacritty..."
+mkdir -p $HOME/.config/alacritty
+create_symlink $DOTFILES/alacritty $HOME/.config/alacritty
 
 # Bash
-echo -e "\tSyncing bash... (default configurations - in ~/.bashrc and ~/.bash_profile - are removed)"
-rm -rf $HOME/.bashrc $HOME/.bash_profile
-ln -s $DOTFILES/bash/bashrc $HOME/.bashrc
-ln -s $DOTFILES/bash/bash_profile $HOME/.bash_profile
+echo -e "\tSyncing bash..."
+create_symlink $DOTFILES/bash/bashrc $HOME/.bashrc
+create_symlink $DOTFILES/bash/bash_profile $HOME/.bash_profile
 
 # ZSH
-echo -e "\tSyncing zsh... (default configurations - in ~/.zshrc and ~/.zprofile - are removed)"
-rm -rf $HOME/.zshrc $HOME/.zprofile
-ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc
-ln -s $DOTFILES/zsh/zprofile $HOME/.zprofile
-ln -s $DOTFILES/zsh/p10k.zsh $HOME/.p10k.zsh
+echo -e "\tSyncing zsh..."
+create_symlink $DOTFILES/zsh/zshrc $HOME/.zshrc
+create_symlink $DOTFILES/zsh/zprofile $HOME/.zprofile
+create_symlink $DOTFILES/zsh/p10k.zsh $HOME/.p10k.zsh
 
 # Xmonad
-echo -e "\tSyncing xmonad... (default configuration - in ~/.xmonad - is removed)"
-rm -rf $HOME/.xmonad
-ln -s $DOTFILES/xmonad $HOME/.xmonad
+echo -e "\tSyncing xmonad..."
+mkdir -p $HOME/.xmonad
+create_symlink $DOTFILES/xmonad $HOME/.xmonad
 chmod +x $HOME/.xmonad/scripts/switchlayout.sh
 chmod +x $HOME/.xmonad/scripts/trayer.sh
 
 # Rofi
 echo -e "\tSyncing rofi..."
-rm -rf $HOME/.config/rofi
-ln -s $DOTFILES/rofi $HOME/.config/rofi
+mkdir -p $HOME/.config/rofi
+create_symlink $DOTFILES/rofi $HOME/.config/rofi
 
 # Picom
 echo -e "\tSyncing picom..."
-rm -rf $HOME/.config/picom
-ln -s $DOTFILES/picom $HOME/.config/picom
+mkdir -p $HOME/.config/picom
+create_symlink $DOTFILES/picom $HOME/.config/picom
 
 # Xmobar
 echo -e "\tSyncing xmobar..."
-rm -rf $HOME/.config/xmobar
-ln -s $DOTFILES/xmobar $HOME/.config/xmobar
+mkdir -p $HOME/.config/xmobar
+create_symlink $DOTFILES/xmobar $HOME/.config/xmobar
 chmod +x $HOME/.config/xmobar/scripts/getlayout.sh
 chmod +x $HOME/.config/xmobar/scripts/trayer-padding-icon.sh
+chmod +x $HOME/.config/xmobar/scripts/getvolume.sh
 
 echo "Add Node LTS to NVM..."
 source /usr/share/nvm/init-nvm.sh
@@ -140,8 +174,10 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 echo -e "\tSyncing nvim..."
 mkdir -p $HOME/.config/nvim
-rm -rf $HOME/.config/nvim/init.vim
-ln -S $DOFTFILES/nvim/init.vim $HOME/.config/nvim/init.vim
+create_symlink $DOFTFILES/nvim/init.vim $HOME/.config/nvim/init.vim
+nvim +"PlugInstall" +qall
+nvim +"CocInstall -sync coc-tsserver coc-json coc-eslint coc-html coc-css" +qall
+nvim +"CocUpdateSync" +qall
 
 echo "Done ! The system will reboot now..."
-sudo reboot
+# sudo reboot
