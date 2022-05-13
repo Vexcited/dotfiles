d37cec0033051edf18f490076a3045e5f54b40a7
Mikkel RINGAUD <mikkel@milescode.dev>
2022-05-13 19:45:08 +0200

    feat(Arch-Linux): add media keys and float for fullscreen apps

--- arch-linux/xmonad/xmonad.hs
+++ arch-linux/xmonad/xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 
 import XMonad.Hooks.WorkspaceHistory
 import XMonad.Hooks.DynamicProperty
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.SetWMName
 
@@ -135,6 +136,15 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- Run xmessage with a summary of the default keybindings (useful for beginners)
     , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
+    
+
+    -- Media keys.
+    , ((0,                 0x1008ff03), spawn "xbacklight -dec 10")
+    , ((0,                 0x1008ff02), spawn "xbacklight -inc 10")
+    , ((0,                 0x1008ff11), spawn "amixer -q sset Master 2%-")
+    , ((0,                 0x1008ff13), spawn "amixer -q sset Master 2%+")
+    , ((0,                 0x1008ff12), spawn "amixer set Master toggle")
+    , ((0,                 0x1008ffb2), spawn "amixer set Mic toggle")
     ]
     ++
 
@@ -199,20 +209,7 @@ myLayout = avoidStruts $ spacing 10 $ (tiled ||| Mirror tiled ||| Full)
      delta   = 3/100
 
 ------------------------------------------------------------------------
--- Window rules:
 
--- Execute arbitrary actions and WindowSet manipulations when managing
--- a new window. You can use this to, for example, always float a
--- particular program, or have a client always appear on a particular
--- workspace.
---
--- To find the property name associated with a program, use
--- > xprop | grep WM_CLASS
--- and click on the client you're interested in.
---
--- To match on the WM_NAME, you can use 'title' in the same way that
--- 'className' and 'resource' are used below.
---
 myManageHook = composeAll
     [ className =? "confirm"       --> doFloat
     , className =? "file_progress" --> doFloat
@@ -220,19 +217,18 @@ myManageHook = composeAll
     , className =? "download"      --> doFloat
     , className =? "error"         --> doFloat
     , className =? "notification"  --> doFloat
-    , className =? "discord"       --> doShift (myWorkspaces !! 4)]
 
-------------------------------------------------------------------------
--- Event handling
+    , className =? "discord"       --> doShift (myWorkspaces !! 4)
+    , className =? "Steam"         --> doShift (myWorkspaces !! 2)
+    
+    , isDialog                     --> doFloat
+    , isFullscreen                 --> doFullFloat
+    ]
+
 myEventHook = dynamicPropertyChange "WM_CLASS"
   $ composeAll [className =? "Spotify" --> doShift (myWorkspaces !! 3)]
 
 ------------------------------------------------------------------------
--- Startup hook
-
--- Perform an arbitrary action each time xmonad starts or is restarted
--- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
--- per-workspace layout choices.
 
 myWallpaperPath :: String
 myWallpaperPath = "~/.vexcited-dotfiles/wallpapers/wallpaper-2.png"
@@ -244,20 +240,13 @@ myStartupHook = do
   spawnOnce "~/.xmonad/scripts/trayer.sh &"
 
 ------------------------------------------------------------------------
--- Now run xmonad with all the defaults we set up.
+
 main :: IO ()
 main = do
   xmproc <- spawnPipe "xmobar -x 0 ~/.config/xmobar/xmobarrc"
   xmonad $ ewmh $ docks $ defaults xmproc
 
--- A structure containing your configuration settings, overriding
--- fields in the default config. Any you don't override, will
--- use the defaults defined in xmonad/XMonad/Config.hs
---
--- No need to modify this.
---
 defaults xmproc = def {
-      -- simple stuff
         terminal           = myTerminal,
         focusFollowsMouse  = myFocusFollowsMouse,
         clickJustFocuses   = myClickJustFocuses,
@@ -267,16 +256,16 @@ defaults xmproc = def {
         normalBorderColor  = myNormalBorderColor,
         focusedBorderColor = myFocusedBorderColor,
 
-      -- key bindings
+      -- Key bindings.
         keys               = myKeys,
         mouseBindings      = myMouseBindings,
 
-      -- hooks, layouts
+      -- Hooks, layouts.
         layoutHook         = myLayout,
         manageHook         = myManageHook,
         handleEventHook    = myEventHook,
         startupHook        = myStartupHook,
-	logHook            =
+	      logHook            =
           workspaceHistoryHook <+> dynamicLogWithPP xmobarPP
             { ppOutput          = \x -> hPutStrLn xmproc x
             , ppCurrent         = xmobarColor "#88C0D0" "" . wrap "<box type=Bottom width=2 mb=0 color=#88C0D0>" "</box>"
@@ -309,12 +298,12 @@ help = unlines ["Modifier key: 'super'.",
     "mod-k          Move focus to the previous window",
     "mod-m          Move focus to the master window",
     "",
-    "-- modifying the window order",
+    "-- Modifying the window order",
     "mod-Return   Swap the focused window and the master window",
     "mod-Shift-j  Swap the focused window with the next window",
     "mod-Shift-k  Swap the focused window with the previous window",
     "",
-    "-- resizing the master/slave ratio",
+    "-- Resizing the master/slave ratio",
     "mod-h  Shrink the master area",
     "mod-l  Expand the master area",
     "",
