cb0c559729b20d200d1b8b025dc8b3a07bb80c46
Mikkel RINGAUD <mikkel@milescode.dev>
2022-05-13 21:17:53 +0200

    fix(Arch-Linux): trayer height depending of my screen hidpi

--- arch-linux/xmonad/scripts/trayer.sh
+++ arch-linux/xmonad/scripts/trayer.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x2E3440 --height 20
+trayer \
+  --edge top \
+  --align right \
+  --widthtype request \
+  --padding 6 \
+  --SetDockType true \
+  --SetPartialStrut true \
+  --expand true \
+  --transparent true \
+  --alpha 0 \
+  --tint 0x2E3440 \
+  --height 24
